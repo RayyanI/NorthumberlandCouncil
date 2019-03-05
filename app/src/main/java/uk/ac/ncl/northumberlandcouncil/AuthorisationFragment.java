64670994816255c77cdb3200fb6b80c0046348ab
Rayyan Iqbal <r.iqbal@ncl.ac.uk>
2019-03-05 13:55:17 +0000

    Base implementation for twitter login

--- app/src/main/java/uk/ac/ncl/northumberlandcouncil/AuthorisationFragment.java
+++ app/src/main/java/uk/ac/ncl/northumberlandcouncil/AuthorisationFragment.java
@@ -1,70 +1,62 @@
 package uk.ac.ncl.northumberlandcouncil;
 
 /* Begin library imports */
+
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+
+import com.twitter.sdk.android.core.Callback;
+
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.google.android.gms.common.SignInButton;
-import com.twitter.sdk.android.core.DefaultLogger;
-import com.twitter.sdk.android.core.Twitter;
-import com.twitter.sdk.android.core.TwitterAuthConfig;
-import com.twitter.sdk.android.core.TwitterConfig;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
 import com.twitter.sdk.android.core.identity.TwitterLoginButton;
 /* End library imports */
 
 /**
  * Handles code for Authorisation Fragment
+ *
  * @author Rayyan Iqbal
  * Created on 21/02/2019
  * Last modified 21/02/2019 (R Iqbal)
  */
 public class AuthorisationFragment extends Fragment {
+    /* Declarations */
+    TwitterLoginButton twitterLoginButton;
 
-
+    /* End Declarations */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        /* Setup activity & possible exceptions */
-        FragmentActivity fragmentActivity;
-        if (getActivity() != null) {
-            fragmentActivity = getActivity();
-        } else {
-            throw new NullPointerException ("Activity undefined");
-        }
-
-        /* Setup twitter config with keys */
-        TwitterConfig config = new TwitterConfig.Builder(fragmentActivity)
-                .logger(new DefaultLogger(Log.DEBUG))
-                .twitterAuthConfig(new TwitterAuthConfig(getString(R.string.com_twitter_sdk_android_CONSUMER_KEY), getString(R.string.com_twitter_sdk_android_CONSUMER_SECRET)))
-                .debug(true)
-                .build();
-        Twitter.initialize(config);
     }
+
     /**
      * Inflate fragment upon selection
-     * @param inflater for instancing xml fragments
-     * @param container container to display content stream
+     *
+     * @param inflater           for instancing xml fragments
+     * @param container          container to display content stream
      * @param savedInstanceState current activity datas store
      * @return display xml view on screen
      */
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view =  inflater.inflate(R.layout.fragment_authorisation, container, false);
+        View view = inflater.inflate(R.layout.fragment_authorisation, container, false);
 
         /* Configure UI */
 
         // Select the buttons //
-        TwitterLoginButton twitterLoginButton = view.findViewById(R.id.twitterLoginButton);
+        twitterLoginButton = view.findViewById(R.id.twitterLoginButton);
         SignInButton signInButton = view.findViewById(R.id.googleLoginButton);
 
         // Style buttons to produce similar widths and heights //
@@ -74,10 +66,60 @@ public class AuthorisationFragment extends Fragment {
         twitterLoginButton.setTextSize(14);
         twitterLoginButton.setTypeface(Typeface.DEFAULT_BOLD);
 
+        /* Handle twitter login */
+        twitterLoginButton.setCallback(new Callback<TwitterSession>() {
+            /**
+             * Actions to do when a login attempt is successful
+             * @param result - call back
+             */
+            @Override
+            public void success(Result<TwitterSession> result) {
+                System.out.println(result.data.getUserName());
+                updateUI(result.data);
 
+            }
+
+            /**
+             * Actions to do when a login attempt is unsuccessfull
+             * @param exception - exception thrown
+             */
+            @Override
+            public void failure(TwitterException exception) {
+                exception.printStackTrace();
+                // Do something on failure
+            }
+        });
 
 
         return view;
 
     }
+
+
+    /**
+     * Handle authorisation responses, passed from this fragment's activity container
+     *
+     * @param requestCode request code (google / twitter / facebook)
+     * @param resultCode  response ( success / fail)
+     * @param data        object containing response data
+     */
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Pass the activity result to the twitter login button
+        twitterLoginButton.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+    /**
+     * Update the user interface upon a successful login
+     *
+     * @param result object containing user information
+     */
+    private void updateUI(TwitterSession result) {
+        twitterLoginButton.setText(result.getUserName());
+        System.out.println(result.getUserName());
+    }
+
+
 }
--- app/src/main/java/uk/ac/ncl/northumberlandcouncil/MainActivity.java
+++ app/src/main/java/uk/ac/ncl/northumberlandcouncil/MainActivity.java
@@ -2,33 +2,58 @@ package uk.ac.ncl.northumberlandcouncil;
 
 
 /* Begin library imports */
+
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
+import android.content.Intent;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.twitter.sdk.android.core.DefaultLogger;
+import com.twitter.sdk.android.core.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterConfig;
+
+import java.util.List;
+
 /* End library imports */
 
 /**
  * Handles the main activity of the application and creates listeners/formats navigation
+ *
  * @author Rayyan Iqbal
  * Created on 20/02/2019
  * Last modified 21/02/2019 (R Iqbal)
  */
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener  {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        /* Create twitter context */
+        TwitterConfig config = new TwitterConfig.Builder(this)
+                .logger(new DefaultLogger(Log.DEBUG))
+                .twitterAuthConfig(new TwitterAuthConfig(getString(R.string.com_twitter_sdk_android_CONSUMER_KEY), getString(R.string.com_twitter_sdk_android_CONSUMER_SECRET)))
+                .debug(true)
+                .build();
+        Twitter.initialize(config);
+        /* End context creation */
+
+
 
         /* Add menu button */
         Toolbar toolbar = findViewById(R.id.toolBar);
@@ -64,10 +89,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     /**
      * Center the logo in the tool bar programmatically
+     *
      * @param hasFocus - whether current window has focus
      */
     @Override
-    public void onWindowFocusChanged(boolean hasFocus){
+    public void onWindowFocusChanged(boolean hasFocus) {
         Toolbar toolbar = findViewById(R.id.toolBar);
         ImageView logo = findViewById(R.id.logo);
         int offset = (toolbar.getWidth() / 2) - (logo.getWidth() / 2);
@@ -75,8 +101,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         logo.setX(offset);
 
     }
+
     /**
      * Handle displayed activity from menu select callback
+     *
      * @param menuItem - item selected
      * @return boolean - upon selection
      */
@@ -100,6 +128,24 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
         drawer.closeDrawer(GravityCompat.START); // Close navigation bar
         return true;
+    }
+
+    /**
+     * Handle activity results and pass them to appropriate fragments
+     *
+     * @param requestCode - integer req code
+     * @param resultCode  - response from server
+     * @param data        - data i.e. login
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        List<Fragment> fragments = getSupportFragmentManager().getFragments();
+        for (Fragment fragment : fragments) {
+            if (fragment instanceof AuthorisationFragment) {
+                fragment.onActivityResult(requestCode, resultCode, data);
+            }
+        }
     }
 
     /**
