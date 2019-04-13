538e0560727bc09f6993b9f22badb31e1bcc2221
Jappy1466 <jaspergriffinjsg@hotmail.co.uk>
2019-04-13 10:54:46 +0100

    Map now automatically zooms on current location + alvin zoom fix

--- app/src/main/java/uk/ac/ncl/northumberlandcouncil/MapFragment.java
+++ app/src/main/java/uk/ac/ncl/northumberlandcouncil/MapFragment.java
@@ -11,6 +11,7 @@ import android.location.Location;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
@@ -44,30 +45,20 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+
 import android.widget.Button;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.TextView;
 import android.support.v4.app.ActivityCompat;
 import android.content.pm.PackageManager;
 import android.widget.Toast;
-
 import static android.content.Context.*;
-import static android.support.v4.content.ContextCompat.getSystemService;
 
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 
 public class MapFragment extends Fragment implements OnMapReadyCallback {
     /* Declarations */
     GoogleMap theMap;
     MapView mapview;
-
-    private GoogleApiClient mGoogleApiClient;
-    private LocationRequest mLocationRequest;
-    private LocationListener locationListener;
-    private Task<Location> loc;
     /* End Declarations */
 
     public MapFragment() {
@@ -130,7 +121,8 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
                                     try {
                                         theMap.clear();
                                         theMap.addMarker(new MarkerOptions().title(loc).position(latLng));
-                                        theMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+                                        //theMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+                                        theMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,13f) );
 
                                     } catch (Exception e) {
                                         e.printStackTrace();
@@ -168,9 +160,8 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
 
         this.theMap = googleMap;
         theMap = googleMap;
-        LatLng myPosition;
 
-   if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
             // here to request the missing permissions, and then overriding
@@ -179,43 +170,40 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
             // to handle the case where the user grants the permission. See the documentation
             // for ActivityCompat#requestPermissions for more details.
             ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-        }
-        else {
+        } else {
             theMap.setMyLocationEnabled(true);
         }
 
-        /*Method below calls the handlesNewLocation which sets the default zoom to the user's current location*/
-
-       loc = LocationServices.getFusedLocationProviderClient(this.requireContext()).getLastLocation();
-        if (loc == null) {
-            LocationServices.getFusedLocationProviderClient(getActivity());
-        }
-//        else {
-//            handleNewLocation();
-//       }
-
-
-
+        handleNewLocation();
     }
-//        public void handleNewLocation(){
-//
-//            double currentLatitude = loc.getLatitude();
-//            double currentLongitude = loc.getLongitude();
-//
-//            LatLng latLng = new LatLng(currentLatitude, currentLongitude);
-//
-//            MarkerOptions options = new MarkerOptions().position(latLng).title("position");
-//            theMap.addMarker(options);
-//            theMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 16f));
-//        }
 
 
+    public void handleNewLocation() {
 
+        LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        String provider = locationManager.getBestProvider(criteria, true);
 
+        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
 
+        Location location = locationManager.getLastKnownLocation(provider);
 
+        if (location != null) {
 
+            double latitude = location.getLatitude();
+            double longitude = location.getLongitude();
+            LatLng currentPosition = new LatLng(latitude, longitude);
+            theMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentPosition ,13f) );
+        }
 
+        else {
+            // if the user doesn't have signal, the default zoom is placed in the centre of Northumberland (zoomed out)
+            LatLng defaultPosition = new LatLng(55.224470, -2.014950);
+            theMap.moveCamera(CameraUpdateFactory.newLatLngZoom(defaultPosition ,8) );
+        }
+    }
 
     public static void closeKeyboard(android.app.Activity activity) {
         InputMethodManager inputManager = (InputMethodManager) activity.getSystemService(INPUT_METHOD_SERVICE);
